f0ae47696fe80770c7d8f61796357aae95933530
sherry13131 <sherry.hyma@gmail.com>
2018-06-08 18:43:35 +0800

    Changed forboled, fbcli version to 0.1.1

--- version/version.go
+++ version/version.go
@@ -5,11 +5,12 @@ package version
 // remember to also update examples/basecoin/tests/cli/rpc.sh
 // TODO improve
 
+// version now is 0.1.1
 const Maj = "0"
 const Min = "1"
-const Fix = "0"
+const Fix = "1"
 
-const Version = "0.1.0"
+const Version = "0.1.1"
 
 // GitCommit set by build flags
 var GitCommit = ""
