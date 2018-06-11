4dbcb37e67d9237e3ca455809d1f79af591bb276
sherry13131 <sherry.hyma@gmail.com>
2018-06-11 18:37:24 +0800

    ...

--- x/contrib/wire.go
+++ x/contrib/wire.go
@@ -11,16 +11,12 @@ func RegisterWire(cdc *wire.Codec) {
 	cdc.RegisterInterface((*Contrib)(nil), nil)
 	cdc.RegisterConcrete(&Invite{}, "contrib/Invite", nil)
 	cdc.RegisterConcrete(&Recommend{}, "contrib/Recommend", nil)
+	cdc.RegisterConcrete(&Vote{}, "contrib/Vote", nil)
+	cdc.RegisterConcrete(&Post{}, "contrib/Post", nil)
 	cdc.RegisterInterface((*Status)(nil), nil)
 	cdc.RegisterConcrete(&InviteStatus{}, "contrib/InviteStatus", nil)
 	cdc.RegisterConcrete(&RecommendStatus{}, "contrib/RecommendStatus", nil)
-	cdc.RegisterConcrete(MsgContrib{}, "forbole/ContribMsg", nil)
-
-	// Vote
-	cdc.RegisterConcrete(&Vote{}, "contrib/Vote", nil)
 	cdc.RegisterConcrete(&VoteStatus{}, "contrib/VoteStatus", nil)
-
-	// Post
-	cdc.RegisterConcrete(&Post{}, "contrib/Post", nil)
 	cdc.RegisterConcrete(&PostStatus{}, "contrib/PostStatus", nil)
+	cdc.RegisterConcrete(MsgContrib{}, "forbole/ContribMsg", nil)
 }
